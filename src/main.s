750f17e78dafa33c213812fefc1b149fcbf61b66
Michael Newman <40316353+Newms226@users.noreply.github.com>
2018-10-11 15:39:15 -0700

    partial subrotuine

--- src/main.s
+++ src/main.s
@@ -1,26 +1,3 @@
-; main.s
-; Runs on any Cortex M processor
-; A very simple first project implementing a random number generator
-; Daniel Valvano
-; May 12, 2015
-
-;  This example accompanies the book
-;  "Embedded Systems: Introduction to Arm Cortex M Microcontrollers",
-;  ISBN: 978-1469998749, Jonathan Valvano, copyright (c) 2015
-;  Section 3.3.10, Program 3.12
-;
-;Copyright 2015 by Jonathan W. Valvano, valvano@mail.utexas.edu
-;   You may use, edit, run or distribute this file
-;   as long as the above copyright notice remains
-;THIS SOFTWARE IS PROVIDED "AS IS".  NO WARRANTIES, WHETHER EXPRESS, IMPLIED
-;OR STATUTORY, INCLUDING, BUT NOT LIMITED TO, IMPLIED WARRANTIES OF
-;MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE APPLY TO THIS SOFTWARE.
-;VALVANO SHALL NOT, IN ANY CIRCUMSTANCES, BE LIABLE FOR SPECIAL, INCIDENTAL,
-;OR CONSEQUENTIAL DAMAGES, FOR ANY REASON WHATSOEVER.
-;For more information about my classes, my research, and my books, see
-;http://users.ece.utexas.edu/~valvano/
-
-
 ; we align 32 bit variables to 32-bits
 ; we align op codes to 16 bits
        THUMB
@@ -30,25 +7,25 @@ M      SPACE   4
                  
        AREA    |.text|, CODE, READONLY, ALIGN=2
        EXPORT  Start
-Start  LDR R2,=M       ; R2 = &M, R2 points to M
-       MOV R0,#1       ; Initial seed
-       STR R0,[R2]     ; M=1
-loop   BL  Random
-       B   loop
-;------------Random------------
-; Return R0= random number
-; Linear congruential generator 
-; from Numerical Recipes by Press et al.
-Random LDR R2,=M    ; R2 = &M, R2 points to M
-       LDR R0,[R2]  ; R0=M
-       LDR R1,=1664525
-       MUL R0,R0,R1 ; R0 = 1664525*M
-       LDR R1,=1013904223
-       ADD R0,R1    ; 1664525*M+1013904223 
-       STR R0,[R2]  ; store M
-       BX  LR
 
+Start
+		LDR		r0, #1
+		BLR		count_ones
 
-       ALIGN      
-       END  
+; Assume that r0 has the word loaded. Places count in r2.
+; WARNING: Code overwrites r0
+; r0 = word
+; r1 = word!
+; r2 = count
+count_ones
+		CMP		r0, #0
+		BEQ		lr			; base case
+		; else
+		SUB		r1, r0, #1
+		AND		r0, r0, r0
+		ADD     r2, r2, #1  ; increment count
+		B		count_ones
+		
+        ALIGN      
+        END  
            

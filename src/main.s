4f41298019e0b1298f48772079cece4642f8a787
Michael Newman <40316353+Newms226@users.noreply.github.com>
2018-10-12 03:25:24 -0700

    close to wroking!!
    Execution cycle is valid, missing some logic somwhere though

--- src/main.s
+++ src/main.s
@@ -2,56 +2,71 @@
 		EXPORT  Start
 		IMPORT 	exp
 		   
-par0	DCD		0x2AAAAA
-par1	DCD		0xCCCCC
-par2	DCD		0x30F0F0
-par3	DCD		0xFF00
-par4	DCD		0x3F0000
+masks	DCD		0x2AAAAA, 0xCCCCC, 0x30F0F0, 0xFF00, 0x3F0000
 	
 parA    DCD		0x3FFFFF
 	
 test1	DCD		0x331B3D
 asnw1	DCD		0x331B7D
-	
+
+; {r0 - r2} -> general purpose
+;
+; r8  -> word to parse
+; r9  -> working address of the mask array
+; r10 -> index of working parity bit in calculated parity word
+; r11 -> calculated parity word
+; r12 -> received Parity word
+
 Start
 		ADR		r0, test1;
 		LDR		r8, [r0];
+		ADR		r9, masks
+		BL		extract_recieved_code
+		MOV		r10, #0
+		B		generic_parity
+	
+calc_correcting_code
+		EORS	r0, r12, r11
+		BEQ		stop
 		
+correct_word
+		MOV		r1, #1
+		LSL		r1, r0
+		EOR		r8, r8, r1
+		
+stop
+		PUSH {r8}
+		B		.
+
+
 extract_recieved_code
 		; word in r8
 		; received code in r12
-		MOV 	r12, #0 		; reset rec_parity_word
+		MOV 	r12, #0 		     ; reset rec_parity_word
 		MOV 	r6, #1
-		MOV 	r0, r8          ; r0 = working space
-		MOV 	r7, #3
+		MOV 	r0, r8               ; r0 = working space
 		AND 	r1, r0, #6
-		MOV 	r12, r1, LSR #1  ; bits 0 & 1 in correct position
-		AND 	r1, r0, r6, LSL #4 ; r1 contains par2
-		ORR 	r12, r12, r1, LSR #2 ; par2 in correct position
-		AND 	r1, r0, r6, LSL #8 ; r1 contains par3
-		ORR 	r12, r12, r1, LSR #5
-		AND 	r1, r0, r6, LSL #16
-		ORR 	r12, r12, r1, LSR #12
-		
-calc_parity_word
-		
-			
-; generic method to calculate parity bit
-;
-; r0  -> general (initially loaded with address of mask)
-; r9  -> index of working parity bit in recieved word
-; r10 -> index of working parity bit in parity word
-; r11 -> calculated parity bit
+		MOV 	r12, r1, LSR #1      ; bits 0 & 1 in correct position
+		AND 	r1, r0, r6, LSL #4   ; r1 contains par2
+		ORR 	r12, r1, LSR #2      ; par2 in correct position
+		AND 	r1, r0, r6, LSL #8   ; r1 contains par3
+		ORR 	r12, r1, LSR #5
+		AND 	r1, r0, r6, LSL #16  
+		ORR 	r12, r1, LSR #12
+		BX		lr
+
 generic_parity
-		TST		r10, #5				; base case
-		BGE		
+		CMP		r10, #5				; base case
+		BEQ		calc_correcting_code
 		
-		LDR  	r0, [r0]            ; load actual mask
-		AND  	r0, r0, r8       	; r0 = unpacked parity
+		LDR  	r0, [r7], #1		; load actual mask
+		AND  	r0, r8       		; r0 = unpacked parity
 		BL	 	count_ones		    ; puts count in r0
-		ANDS 	r0, r0, #1          ; Zero flag will be set if count is even
+		ANDS 	r0, #1          	; Zero flag will be set if count is even
 		BEQ		shift				; Branch to shift instruction 
-		ADD		r10, r10, #1		; increment index
+		
+continue
+		ADD		r10, #1				; increment index
 		B		generic_parity
 
 shift
@@ -63,19 +78,7 @@ shift
 		AND		r0, r0, #1          ; r0 = unpacked parity bit
 		LSL		r0, r10             ; move parity bit to position
 		ORR		r11, r11, r0        ; pack calculated parity bit
-		BX		lr
-
-
-end_loop
-calc_correcting_code
-		EORS	r0, r12, r11
-		BEQ		stop
-		MOV		r1, #1
-		LSL		r1, r0
-		EOR		r8, r8, r1
-		
-stop
-		B		.
+		B		continue
 
 ;parity_all_check
 ;		ADR		r1, par1
